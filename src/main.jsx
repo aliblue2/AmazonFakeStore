127297c488a72d003369b4cda7c017650d86991d
alireza <intertik.ir@gmail.com>
2024-02-10 12:46:24 +0330

    added-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Account from "./pages/Account"
 import Cart from "./pages/Cart"
@@ -8,12 +8,12 @@ import Login from "./pages/Login"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
   <Routes>
     <Route element={<Home />} index path={"/"}/>
     <Route element={<Account />} path={"/acoount"} />
     <Route element={<Cart />} path={"/cart"} />
     <Route element={<Login />} path={"/login"} />
   </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/AmazonFakeStore/",
   plugins: [react()],
-  server:{
-    host : true
-  }
+  
 })
